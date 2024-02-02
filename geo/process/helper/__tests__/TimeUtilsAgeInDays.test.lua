ef677a1c9a22532fc89ee8da01f4ff58af4549c1
rush42 <laurenz@fixmycity.de>
2024-02-02 13:51:54 +0100

    update include paths: `aop` -> `geo`

--- geo/process/helper/__tests__/TimeUtilsAgeInDays.test.lua
+++ geo/process/helper/__tests__/TimeUtilsAgeInDays.test.lua
@@ -1,4 +1,4 @@
-package.path = package.path .. ";app/process/helper/?.lua"
+package.path = package.path .. ";/geo/process/helper/?.lua"
 require("TimeUtils")
 
 print('=== Test TimeUtils AgeInDays ===')
