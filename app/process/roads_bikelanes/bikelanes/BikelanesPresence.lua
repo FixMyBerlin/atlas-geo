bc5d47cdf4de4a937fae586e56408888920358e3
rush42 <laurenz@fixmycity.de>
2024-02-19 13:23:33 +0100

    assumed_no for bundesstraßen and onesided complete

--- app/process/roads_bikelanes/bikelanes/BikelanesPresence.lua
+++ app/process/roads_bikelanes/bikelanes/BikelanesPresence.lua
@@ -15,13 +15,24 @@ function BikelanesPresence(object, cycleways)
 
   -- Filter ways where we dont expect bicycle infrastructure
   -- TODO: filter on surface and traffic zone and maxspeed (maybe wait for maxspeed PR)
-  local infrastrucuteNotExpected = Set({ "residential", "road", "living_street" })
+  local infrastrucuteNotExpected = Set({ "residential", "road", "living_street", 'bicycle_road', 'pedestrian',
+    'residential_priority_road', 'unspecified_road', 'service_road', 'service_alley', 'service_unkown' })
+  local noInfrastructureOnSelfAssumed = Set({ 'unclassified', 'primary', 'tertiary_link', 'secondary_link', 'tertiary',
+    'secondary', 'primary_link' })
+
   if infrastrucuteNotExpected[tags.highway] or presence[CENTER_SIGN] then
     -- set the nil values to 'not_expected', for all minor roads and complete data
     for _, side in pairs(sides) do presence[side] = presence[side] or NOT_EXPECTED end
   end
-  if (presence[RIGHT_SIGN] or presence[LEFT_SIGN]) then
+  if presence[RIGHT_SIGN] or presence[LEFT_SIGN] then
     presence[CENTER_SIGN] = presence[CENTER_SIGN] or NOT_EXPECTED
+  end
+  if noInfrastructureOnSelfAssumed[tags.highway] then
+    presence[CENTER_SIGN] = presence[CENTER_SIGN] or 'assumed_no'
+    if (presence[LEFT_SIGN] and presence[LEFT_SIGN] ~= 'data_no') or (presence[RIGHT_SIGN] and presence[RIGHT_SIGN] ~= 'data_no') then
+      presence[LEFT_SIGN] = presence[LEFT_SIGN] or 'assumed_no'
+      presence[RIGHT_SIGN] = presence[RIGHT_SIGN] or 'assumed_no'
+    end
   end
   if tags.oneway == 'yes' and tags['oneway:bicycle'] ~= 'no' then
     presence[LEFT_SIGN] = presence[LEFT_SIGN] or NOT_EXPECTED
