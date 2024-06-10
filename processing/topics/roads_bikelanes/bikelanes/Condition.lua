db3a6d47cd11808d9a50bd9d43ca1fcaa0d14f70
Laurenz Rasche <laurenz@fixmycity.de>
2024-06-10 19:36:18 +0200

    order code by class and inherit metamethods

--- processing/topics/roads_bikelanes/bikelanes/Condition.lua
+++ processing/topics/roads_bikelanes/bikelanes/Condition.lua
@@ -1,6 +1,16 @@
 package.path = package.path .. ";/processing/topics/helper/?.lua"
 require("Set")
 
+local _metamethods = { -- all metamethods except __index
+  '__add', '__call', '__concat', '__div', '__le', '__lt', '__mod', '__mul', '__pow', '__sub', '__tostring', '__unm'
+}
+
+local function setmetamethods(class)
+  local superClass = getmetatable(class)
+  for _, mm in pairs(_metamethods) do
+    class[mm] = superClass[mm]
+  end
+end
 --- @class Condition
 --- This class is an abstract class from which specific implementatios can inhert from.
 --- It implements only the two function `__add` and `__mul`.
@@ -14,38 +24,30 @@ function Condition:new(name)
   return cond
 end
 
--- JUNCTORS: are used to combine different conditions
---- @class Conjunction
---- This class implements the conjunction of two Conditions `A` and `B` such that it evaluates to `A:eval() and B:eval()`
-Conjunction = Condition:new()
-function Conjunction:new(A, B)
-  local conj = {A = A, B = B}
-  setmetatable(conj, self)
-  self.__index = self
-  return conj
+function Condition.__unm(cond)
+  return Negation:new(cond)
 end
 
-function Conjunction:eval(x)
-  return self.A:eval(x) and self.B:eval(x)
+function Condition.__add(A, B)
+  return Disjunction:new(A, B)
 end
 
---- @class Disjunction
---- This class implements the disjunction of two Conditions `A` and `B` such that it evaluates to `A:eval() or B:eval()`
-Disjunction = Condition:new()
-function Disjunction:new(A, B)
-  local disj = {A = A, B = B}
-  setmetatable(disj, self)
-  self.__index = self
-  return disj
+function Condition.__mul(A, B)
+  if getmetatable(A) == Disjunction or getmetatable(B) == Disjunction then
+    return Disjunction.__mul(A, B)
+  end
+  return Conjunction:new(A, B)
 end
 
-function Disjunction:eval(x)
-  return self.A:eval(x) or self.B:eval(x)
+function Condition:__tostring()
+  return self.name
 end
 
 --- @class Negation
 --- This class implements the negation of a Condition `C` such that it evaluates to `not C:eval()`
 Negation = Condition:new()
+setmetamethods(Negation)
+
 function Negation:new(condition)
   local neg = {condition = condition}
   setmetatable(neg, self)
@@ -57,29 +59,66 @@ function Negation:eval(x)
   return not self.condition:eval(x)
 end
 
-function Condition.__unm(cond)
-  return Negation:new(cond)
-end
-
--- double negation equals the original condition
 function Negation.__unm(neg)
   return neg.condition
 end
 
--- DeMorgan rules
+function Negation:__tostring()
+  return "¬" .. self.condition:__tostring()
+end
+
+--- @class Conjunction
+--- This class implements the conjunction of two Conditions `A` and `B` such that it evaluates to `A:eval() and B:eval()`
+Conjunction = Condition:new()
+setmetamethods(Conjunction)
+
+function Conjunction:new(A, B)
+  local conj = {A = A, B = B}
+  setmetatable(conj, self)
+  self.__index = self
+  return conj
+end
+
+function Conjunction:eval(x)
+  return self.A:eval(x) and self.B:eval(x)
+end
+
 function Conjunction.__unm(conj)
   return Disjunction:new(-conj.A, -conj.B)
+end
+
+function Conjunction:__tostring()
+  local stringA = self.A:__tostring()
+  local stringB = self.B:__tostring()
+  if getmetatable(self.A) == Disjunction then
+    stringA = '(' .. stringA .. ')'
+  end
+  if getmetatable(self.B) == Disjunction then
+    stringB = '(' .. stringB .. ')'
+  end
+  return stringA .. ' ∧ ' .. stringB
+end
+
+--- @class Disjunction
+--- This class implements the disjunction of two Conditions `A` and `B` such that it evaluates to `A:eval() or B:eval()`
+Disjunction = Condition:new()
+setmetamethods(Disjunction)
+
+function Disjunction:new(A, B)
+  local disj = {A = A, B = B}
+  setmetatable(disj, self)
+  self.__index = self
+  return disj
 end
 
 function Disjunction.__unm(disj)
   return Conjunction:new(-disj.A, -disj.B)
 end
 
-function Condition.__add(A, B)
-  return Disjunction:new(A, B)
+function Disjunction:eval(x)
+  return self.A:eval(x) or self.B:eval(x)
 end
 
--- Assoziativity
 function Disjunction.__mul(A, B)
   if getmetatable(A) == getmetatable(B) then
     return Disjunction:new(Disjunction:new(A.A * B.A, A.B * B.A), Disjunction:new(A.A * B.B, A.B * B.B))
@@ -93,19 +132,6 @@ function Disjunction.__mul(A, B)
   error("This should never happen")
 end
 
-function Condition.__mul(A, B)
-  return Conjunction:new(A, B)
-end
-
--- tostring methods
-function Condition:__tostring()
-  return self.name
-end
-
-function Negation:__tostring()
-  return "¬" .. self.condition:__tostring()
-end
-
 function Disjunction:__tostring()
   local stringA = self.A:__tostring()
   local stringB = self.B:__tostring()
@@ -116,19 +142,6 @@ function Disjunction:__tostring()
     stringB = '(' .. stringB .. ')'
   end
   return stringA .. ' ∨ ' .. stringB
-end
-
-function Conjunction:__tostring()
-  local stringA = self.A:__tostring()
-  local stringB = self.B:__tostring()
-  if getmetatable(self.A) == Disjunction then
-    stringA = '(' .. stringA .. ')'
-  end
-  if getmetatable(self.B) == Disjunction then
-    stringB = '(' .. stringB .. ')'
-  end
-  return stringA .. ' ∧ ' .. stringB
-
 end
 
 -- PREDICATES: are conditions specific to our data
